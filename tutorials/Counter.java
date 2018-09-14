52174d196ccaa7598508ca4bb72ca39c27385d1d
Taufiq Gh <jtaufiq1@gmail.com>
2018-09-14 18:47:52 +0000

    Create classes and Package

--- tutorials/Counter.java
+++ tutorials/Counter.java
@@ -1,14 +1,13 @@
 /* Static Class and functions */
 public class Counter {
-    static int count = 0;
-    static void counter() {
+    private static int count = 0;
+    public Counter() {
         count++;
     }
     public static void main(String[] args) {
-        // Calling Static function 
-        for(int i = 0; i < 5; ++i) {
-            counter();
-            System.out.println(count);
-        }
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+
+        System.out.println(count);
     }
 }
