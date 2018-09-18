ba911fdb338da2ee2710dc6a9ce3dec5f916e840
Taufiq Gh <jtaufiq1@gmail.com>
2018-09-18 19:55:45 +0000

    Getting user input

---
+++ tutorials/SysInp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class SysInput {
+	
+	public static void main(String[ ] args) {
+	
+		Scanner inp = new Scanner(System.in);
+		int i = inp.nextInt();
+
+		System.out.println(i);
+	}
+} 
